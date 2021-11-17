ed300c2b0aa60c402e8953220dc7c75bc0a75f10
davidbaillydev <david.bailly8@gmail.com>
2021-11-17 13:38:34 +0100

    ok

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCsgb5Ri0DRK9gE0nJ0PYCh9BLByhbJatE",
-  authDomain: "rn-uber-eats-clone-yt.firebaseapp.com",
-  projectId: "rn-uber-eats-clone-yt",
-  storageBucket: "rn-uber-eats-clone-yt.appspot.com",
-  messagingSenderId: "791658363133",
-  appId: "1:791658363133:web:91e4fe1be6f3f23467a694",
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAGWjyCb4V5to0igDsconS2YuYqaQbotRQ",
+  authDomain: "uber-eats-332321.firebaseapp.com",
+  projectId: "uber-eats-332321",
+  storageBucket: "uber-eats-332321.appspot.com",
+  messagingSenderId: "461239992069",
+  appId: "1:461239992069:web:28a13e5e5e12497848c16c"
+};
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
 export default firebase;
+
